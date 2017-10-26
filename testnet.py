e40623098741be6b011b9cc10564655a48b6c54c
Brian Liotti <bliotti@protonmail.com>
2017-10-25 21:14:11 -0400

    Create testnet.py

---
+++ testnet.py
@@ -0,0 +1,33 @@
+
+#testnet_generator
+
+from pycoin.ecdsa import generator_secp256k1 as g
+from binascii import hexlify,unhexlify
+from pycoin.encoding import b2a_hashed_base58,hash160
+
+secret = 22334455
+x,y = (secret*g).pair()
+pub = g.__class__(g.curve(), x,y)
+hex_x = hex(x)
+hex_x = hex_x[2:]
+hex_x = hex_x[:-1]
+hex_y = hex(y)
+hex_y = hex_y[2:]
+hex_y = hex_y[:-1]
+y % 2 #odd
+
+#compressed and y % 2 =1 so odd -> preprend 03
+sec = '03' + hex_x
+sec_bin = unhexlify(sec)
+h160 = hash160(sec_bin)
+address_unicode = b2a_hashed_base58(chr(0x6f) + h160)
+address = str(address_unicode)
+
+#uncompressed
+sec = '04' + hex_x + hex_y
+sec_bin = unhexlify(sec)
+h160 = hash160(sec_bin)
+addres  = str(b2a_hashed_base58(chr(0x6f) + h160))
+
+
+address = 'mo3kHhHn6ixsXy8LGcdY2zW2QkE8FmhsFH'
